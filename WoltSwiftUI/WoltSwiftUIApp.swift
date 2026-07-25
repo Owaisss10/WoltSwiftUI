a3c1064b38c6f90bfd8e00d52a39a8b885b6ee4b
Awais Akram <owaisakram103@gmail.com>
2026-07-26 00:23:00 +0300

    Initial commit - Empty SwiftUI Project

---
+++ WoltSwiftUI/WoltSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoltSwiftUIApp.swift
+//  WoltSwiftUI
+//
+//  Created by Awais Akram on 26.7.2026.
+//
+
+import SwiftUI
+
+@main
+struct WoltSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
